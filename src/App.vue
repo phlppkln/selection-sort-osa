d3fbe95bbe4c2dd23d35646f84af54ee5da1b1ac
phlppkln <philipp.klein2@gmx.at>
2021-07-26 16:22:47 +0200

    create card component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<ul>
+  <li><Card :number="numbers[0]"/></li>
+  <li><Card :number="numbers[1]"/></li>
+  <li><Card :number="numbers[2]"/></li>
+  <li><Card :number="numbers[3]"/></li>
+  <li><Card :number="numbers[4]"/></li>
+</ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card
+  },
+  data(){
+    return{
+      numbers: [],
+      readActions: 0, /*lesen*/
+      saveActions: 0, /*merken*/
+      fixActions: 0, /*fixieren*/
+      startTime: null,
+      endTime: null
+    };
+  },
+  created(){
+    this.numbers.push(Math.random());
+    this.numbers.push(Math.random());
+    this.numbers.push(Math.random());
+    this.numbers.push(Math.random());
+    this.numbers.push(Math.random());
+    console.log(this.numbers);
+  },
+  methods: {
+    createRandomNumbers(){
+      console.log("getRandomNumber()")
+      return Math.random();
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS';
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card">
+      <div class="fixieren">
+          <div class="number">{{ number }}</div>
+          <div class="lesen-btn" >Lesen</div>
+          <div class="merken-btn">Merken</div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+     props: ['number'],
+
+}
+</script>
+
+<style>
+.card{
+    background-color: gray;
+    width: 100px;
+    height: 100px;
+}
+</style>
